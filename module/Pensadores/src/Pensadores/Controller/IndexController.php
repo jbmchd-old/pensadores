cd3af31a915ce55d847d090058cb97e8f36a993c
jb <jb@Joabe-Casa>
2015-05-03 23:39:14 -0300

    * exibicao das colunas no pagina principal * limitada a quantidade maxima de colunas a serem exibidas * adicionada acao para os botoes de avançar colunas e voltar colunas

--- module/Pensadores/src/Pensadores/Controller/IndexController.php
+++ module/Pensadores/src/Pensadores/Controller/IndexController.php
@@ -13,27 +13,61 @@ class IndexController extends ControllerGenerico{
         $colunas_raw = $srv_vcolunas->getAllOrderByCtgIdAndColDataPostagemDesc();
         
         $colunas = [];
-        foreach ($colunas_raw as $key => $cada_coluna) {
+        foreach ($colunas_raw as $cada_coluna) {
             $ctg_id = $cada_coluna->getCtgId();
+            $col_id = $cada_coluna->getColId();
             $imagem_coluna = $cada_coluna->getColEndImagem();
             if(empty(trim($imagem_coluna))){
                 $imagem_coluna_end = $this->basePath().'img/colunas/imagem_padrao_coluna.jpg';
             }
             
-            $colunas[$ctg_id][$key]['col_id'] = $cada_coluna->getColId();
-            $colunas[$ctg_id][$key]['col_titulo'] = ucfirst(mb_strtolower($cada_coluna->getColTitulo()));
-            $colunas[$ctg_id][$key]['col_texto'] = $cada_coluna->getColTexto();
-            $colunas[$ctg_id][$key]['col_data_postagem'] = $cada_coluna->getColDataPostagem();
-            $colunas[$ctg_id][$key]['col_resumo'] = $cada_coluna->getColResumo();
-            $colunas[$ctg_id][$key]['col_end_imagem'] = $imagem_coluna_end;
-            $colunas[$ctg_id][$key]['chv_id'] = $cada_coluna->getChvId();
-            $colunas[$ctg_id][$key]['chv_nome'] = $cada_coluna->getChvNome();
-            $colunas[$ctg_id][$key]['ctg_id'] = $cada_coluna->getCtgId();
-            $colunas[$ctg_id][$key]['ctg_nome'] = $cada_coluna->getCtgNome();
-            $colunas[$ctg_id][$key]['ctg_apelido'] = $cada_coluna->getCtgApelido();
-            $colunas[$ctg_id][$key]['usr_id'] = $cada_coluna->getUsrId();
-            $colunas[$ctg_id][$key]['usr_nome'] = $cada_coluna->getUsrNome();
-            $colunas[$ctg_id][$key]['usr_sobrenome'] = $cada_coluna->getUsrSobrenome();
+            $colunas[$ctg_id][$col_id]['col_id']            = $cada_coluna->getColId();
+            $colunas[$ctg_id][$col_id]['col_titulo']        = ucfirst(mb_strtolower($cada_coluna->getColTitulo()));
+            $colunas[$ctg_id][$col_id]['col_texto']         = $cada_coluna->getColTexto();
+            $colunas[$ctg_id][$col_id]['col_data_postagem'] = $cada_coluna->getColDataPostagem();
+            $colunas[$ctg_id][$col_id]['col_resumo']        = $cada_coluna->getColResumo();
+            $colunas[$ctg_id][$col_id]['col_end_imagem']    = $imagem_coluna_end;
+            $colunas[$ctg_id][$col_id]['chv_id']            = $cada_coluna->getChvId();
+            $colunas[$ctg_id][$col_id]['chv_nome']          = $cada_coluna->getChvNome();
+            $colunas[$ctg_id][$col_id]['ctg_id']            = $cada_coluna->getCtgId();
+            $colunas[$ctg_id][$col_id]['ctg_nome']          = $cada_coluna->getCtgNome();
+            $colunas[$ctg_id][$col_id]['ctg_apelido']       = $cada_coluna->getCtgApelido();
+            $colunas[$ctg_id][$col_id]['usr_id']            = $cada_coluna->getUsrId();
+            $colunas[$ctg_id][$col_id]['usr_nome']          = $cada_coluna->getUsrNome();
+            $colunas[$ctg_id][$col_id]['usr_sobrenome']     = $cada_coluna->getUsrSobrenome();
+            
+            $col_id += 5;
+            $colunas[$ctg_id][$col_id]['col_id']            = $cada_coluna->getColId();
+            $colunas[$ctg_id][$col_id]['col_titulo']        = ucfirst(mb_strtolower($cada_coluna->getColTitulo()));
+            $colunas[$ctg_id][$col_id]['col_texto']         = $cada_coluna->getColTexto();
+            $colunas[$ctg_id][$col_id]['col_data_postagem'] = $cada_coluna->getColDataPostagem();
+            $colunas[$ctg_id][$col_id]['col_resumo']        = $cada_coluna->getColResumo();
+            $colunas[$ctg_id][$col_id]['col_end_imagem']    = $imagem_coluna_end;
+            $colunas[$ctg_id][$col_id]['chv_id']            = $cada_coluna->getChvId();
+            $colunas[$ctg_id][$col_id]['chv_nome']          = $cada_coluna->getChvNome();
+            $colunas[$ctg_id][$col_id]['ctg_id']            = $cada_coluna->getCtgId();
+            $colunas[$ctg_id][$col_id]['ctg_nome']          = $cada_coluna->getCtgNome();
+            $colunas[$ctg_id][$col_id]['ctg_apelido']       = $cada_coluna->getCtgApelido();
+            $colunas[$ctg_id][$col_id]['usr_id']            = $cada_coluna->getUsrId();
+            $colunas[$ctg_id][$col_id]['usr_nome']          = $cada_coluna->getUsrNome();
+            $colunas[$ctg_id][$col_id]['usr_sobrenome']     = $cada_coluna->getUsrSobrenome();
+            
+            $col_id += 5;
+            $colunas[$ctg_id][$col_id]['col_id']            = $cada_coluna->getColId();
+            $colunas[$ctg_id][$col_id]['col_titulo']        = ucfirst(mb_strtolower($cada_coluna->getColTitulo()));
+            $colunas[$ctg_id][$col_id]['col_texto']         = $cada_coluna->getColTexto();
+            $colunas[$ctg_id][$col_id]['col_data_postagem'] = $cada_coluna->getColDataPostagem();
+            $colunas[$ctg_id][$col_id]['col_resumo']        = $cada_coluna->getColResumo();
+            $colunas[$ctg_id][$col_id]['col_end_imagem']    = $imagem_coluna_end;
+            $colunas[$ctg_id][$col_id]['chv_id']            = $cada_coluna->getChvId();
+            $colunas[$ctg_id][$col_id]['chv_nome']          = $cada_coluna->getChvNome();
+            $colunas[$ctg_id][$col_id]['ctg_id']            = $cada_coluna->getCtgId();
+            $colunas[$ctg_id][$col_id]['ctg_nome']          = $cada_coluna->getCtgNome();
+            $colunas[$ctg_id][$col_id]['ctg_apelido']       = $cada_coluna->getCtgApelido();
+            $colunas[$ctg_id][$col_id]['usr_id']            = $cada_coluna->getUsrId();
+            $colunas[$ctg_id][$col_id]['usr_nome']          = $cada_coluna->getUsrNome();
+            $colunas[$ctg_id][$col_id]['usr_sobrenome']     = $cada_coluna->getUsrSobrenome();
+            
             
         }
         
