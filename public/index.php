f3deff2430186064a39abda0c6902f8279b51e38
Joabe <dpcbi02@outlook.com>
2015-06-01 09:17:21 -0300

    desabilitando algumas partes momentaneamente

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
   * Display all errors when APPLICATION_ENV is development.
   */
 
- if ($_SERVER['ENVIROMENT'] == 'development') {
+ if (getenv('APPLICATION_ENV') == 'development') {
     mb_internal_encoding('UTF-8');
     error_reporting(E_ALL);
     ini_set("display_errors",2);
