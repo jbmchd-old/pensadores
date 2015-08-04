5ecdaec765d944165b1f9c27e2f64b015ca0d09a
Joabe Machado <jb.machado42@gmail.com>
2015-08-03 21:00:10 -0300

    * exibindo pagina do colunista

--- module/Coluna/src/Coluna/Entity/View/VColColuna.php
+++ module/Coluna/src/Coluna/Entity/View/VColColuna.php
@@ -6,35 +6,6 @@ use Nucleo\Service\GenericEntity;
 
 class VColColuna extends GenericEntity {
 	
-    private $usrId;
-    private $ctgId;
-    private $chvId;
-    private $colId;
-    private $colTitulo;
-    private $colTexto;
-    private $colDataPostagem;
-    private $colDataModificacao;
-    private $colResumo;
-    private $colObservacao;
-    private $colEndImagem;
-    private $colStatus;
-    private $serId;
-    private $serNome;
-    private $colPaiId;
-    private $serOrdem;
-    private $chvNome;
-    private $ctgNome;
-    private $ctgApelido;
-    private $usrLogin;
-    private $usrEmail;
-    private $usrNome;
-    private $usrSobrenome;
-    private $usrGenero;
-    private $usrDataNascimento;
-    private $usrDataCadastro;
-    private $usrFoto;
-    private $usrStatus;
-
     public function setColDataPostagem($colDataPostagem){
         $this->colDataPostagem = (new \DateTime($colDataPostagem))->format("d/m/y");
     }
